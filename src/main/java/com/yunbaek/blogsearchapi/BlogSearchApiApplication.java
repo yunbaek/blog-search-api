ca49992230dc2f5d20b732b14b0699dc06dcbf39
yunbaek <yunbaek4564@gmail.com>
2023-03-19 11:54:58 +0900

    Initial commit

---
+++ src/main/java/com/yunbaek/blogsearchapi/BlogSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.yunbaek.blogsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunbaek/blogsearchapi/BlogSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunbaek.blogsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
